a6234aaacd65b3dd3563778248d086c98a6f409a
Khrystyna <saikyyno@gmail.com>
2026-05-18 00:59:29 +0300

    posthg

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import posthog from 'posthog-js';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// Initialize PostHog
+posthog.init('ph_project_api_key', {
+    api_host: 'https://app.posthog.com', // Change to 'https://eu.posthog.com' if your project is hosted in the EU region
+    person_profiles: 'identified_only',
+
+    session_recording: {
+        maskAllInputs: true,
+        maskInputOptions: { password: true },
+    },
+});
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+);
